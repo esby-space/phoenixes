9cb1ac0d534825c89e7f7c4d01a400b2fdb7183c
esby-space <77418085+esby-space@users.noreply.github.com>
2022-08-14 19:00:41 -0400

    i guess this makes more sense, maybe?

--- scripts/seasons.ts
+++ scripts/seasons.ts
@@ -1,7 +1,7 @@
 const addImages = (container: HTMLElement, pathFn: (i: number) => string, number: number) => {
     const columns = Array.from(container.querySelectorAll(".col"));
     let current = 0;
-    for (let i = 1; i <= number; i++) {
+    for (let i = 0; i < number; i++) {
         columns[current].innerHTML += pathFn(i);
         current = (current + 1) % columns.length;
     }
@@ -9,7 +9,7 @@ const addImages = (container: HTMLElement, pathFn: (i: number) => string, number
 
 const initGallery = () => {
     let section2022 = document.querySelector("#section2022") as HTMLElement;
-    addImages(section2022, (i) => `<img src="./images/2022/image${i}.png" class="rounded img-fluid">`, 20);  
+    addImages(section2022, (i) => `<img src="./images/2022/image${i + 1}.png" class="rounded img-fluid">`, 20);  
 };
 
 initGallery();
